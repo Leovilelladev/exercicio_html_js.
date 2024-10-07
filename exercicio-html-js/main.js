f22b36be1917822b01e726c2257ab989ae536d6c
Leovilelladev <164637673+Leovilelladev@users.noreply.github.com>
2024-10-07 10:23:53 -0300

    correção e adição no codigo

--- exercicio-html-js/main.js
+++ exercicio-html-js/main.js
@@ -1,37 +1,41 @@
 const form = document.getElementById('meuFormulario');
 let campoA = document.getElementById('campoA');
 let campoB = document.getElementById('campoB');
-const arrayCampo = [campoA, campoB];
-console.log(arrayCampo[0]);
 
+const mensagemSucesso = "O valor B está maior que o A, por isso está adequado.";
+const mensagemError = "O valor B está menor que o A, por isso operação falhou.";
 
-function validaCampo() {
-    let v1 = arrayCampo[0];
-    let v2 = arrayCampo[1];
-    return v1 > v2
-}
-
-    form.addEventListener('submit', function(e) {
-        let ValidaECampo = false;
-        e.preventDefault();
-
-        const mensagemSucesso = "O valor B está maior que o A, por isso está adequado.";
-        const mensagemError = "O valor B está menor que o A, por isso operação falhou.";
-
-        ValidaECampo = validaCampo (arrayCampo.value)
-    if (ValidaECampo) {
-        const containerMensagemSucesso = document.querySelector('#sucess-message');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
-        campoA.value ='';
-        campoB.value ='';
-
-    } else {
-        const containerMensagemErro = document.querySelector('#error-message')
-        campoA.style.border = '1px solid red';
-        document.querySelector('#error-message');
-        containerMensagemErro.innerHTML = mensagemError;
-        containerMensagemErro.style.display = 'block'
-    
-        }
+form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        verificar();
     })
+
+
+    function verificar(){
+        let valorA = parseFloat(campoA.value);
+        let valorB = parseFloat(campoB.value);
+
+
+        if(valorB > valorA){
+            const containerMensagemSucesso = document.querySelector('#sucess-message');
+            containerMensagemSucesso.innerHTML = mensagemSucesso;
+            containerMensagemSucesso.style.display = 'block';
+
+            document.querySelector('#error-message').style.display = 'none';
+
+            campoA.value = '';
+            campoB.value = '';
+            campoA.style.border = '';
+            campoB.style.border = '';
+        }
+        else {
+            const containerMensagemErro = document.querySelector('#error-message');
+            containerMensagemErro.innerHTML = mensagemError;
+            containerMensagemErro.style.display = 'block';
+
+            document.querySelector('#sucess-message').style.display = 'none';
+
+            campoA.style.border = '1px solid red';
+            campoB.style.border = '1px solid red';
+        }
+    }
